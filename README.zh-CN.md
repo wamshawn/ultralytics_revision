7a0bb052ab8ae81428d5d68f14baff222a5637b2
shawn <ryougi.nevermore@hotmail.com>
2026-03-03 15:23:48 +0800

    add export onnx for rknn

--- README.zh-CN.md
+++ README.zh-CN.md
@@ -19,6 +19,10 @@
 </div>
 <br>
 
+增加导出`rknn`适配的`onnx`，使用`onnx_rknn`格式。支持`Detect`, `Segment`, `Obb` 和 `Pose`.
+
+<br>
+
 [Ultralytics](https://ultralytics.com) [YOLOv8](https://github.com/ultralytics/ultralytics) 是一款前沿、最先进（SOTA）的模型，基于先前 YOLO 版本的成功，引入了新功能和改进，进一步提升性能和灵活性。YOLOv8 设计快速、准确且易于使用，使其成为各种物体检测与跟踪、实例分割、图像分类和姿态估计任务的绝佳选择。
 
 我们希望这里的资源能帮助您充分利用 YOLOv8。请浏览 YOLOv8 <a href="https://docs.ultralytics.com/">文档</a> 了解详细信息，在 <a href="https://github.com/ultralytics/ultralytics/issues/new/choose">GitHub</a> 上提交问题以获得支持，并加入我们的 <a href="https://ultralytics.com/discord">Discord</a> 社区进行问题和讨论！
--- ultralytics/nn/autobackend.py
+++ ultralytics/nn/autobackend.py
@@ -121,6 +121,7 @@ class AutoBackend(nn.Module):
             paddle,
             ncnn,
             triton,
+            onnx_rknn,
         ) = self._model_type(w)
         fp16 &= pt or jit or onnx or xml or engine or nn_module or triton  # FP16
         nhwc = coreml or saved_model or pb or tflite or edgetpu  # BHWC formats (vs torch BCWH)
@@ -556,6 +557,10 @@ class AutoBackend(nn.Module):
         elif self.triton:
             im = im.cpu().numpy()  # torch to numpy
             y = self.model(im)
+
+        # RKNN(ONNX)
+        elif getattr(self, 'onnx_rknn', False):
+            assert "for inference, please refer to https://github.com/airockchip/rknn_model_zoo/"
 
         # TensorFlow (SavedModel, GraphDef, Lite, Edge TPU)
         else:
